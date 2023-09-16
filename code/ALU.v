4022537271812de7ed1ff6a56bb1679652b3e157
RapidRoger18 <Atharvakashalkar1804@gmail.com>
2023-09-16 10:31:35 +0530

    Added M instructions in ALU module

--- code/ALU.v
+++ code/ALU.v
@@ -26,32 +26,41 @@ input [31:0] rs1,
 input [31:0] rs2,
 input [11:0] imm,
 input [31:0] PC,
-input [38:0] instructions, //subjected to change
+input [46:0] instructions, //subjected to change
 output reg [31:0] ALUoutput
     );
 
 always@(instructions) begin
     
         case(instructions)
-            39'h1 : ALUoutput = rs1 + rs2;                                       //add
-            39'h2 : ALUoutput <= rs1 - rs2;                                       //sub
-            39'h4 : ALUoutput <= rs1 ^ rs2;                                       //xor
-            39'h8 : ALUoutput <= rs1 | rs2;                                       //or
-            39'h10 : ALUoutput <= rs1 & rs2;                                      //and
-            39'h20 : ALUoutput <= rs1 << rs2;                                     //sll
-            39'h40 : ALUoutput <= rs1 >> rs2;                                     //srl
-            39'h80 : ALUoutput <= rs1 > rs2;                                      //sra 
-            39'h100 : ALUoutput <= (rs1 > rs2)?1:0;                               //slt
-            39'h200 : ALUoutput <= (rs1 > rs2)?1:0;                               //sltu
-            39'h400 : ALUoutput <= (rs1 + imm);                                   //addi
-            39'h800 : ALUoutput <= (rs1 ^ imm);                                   //xori
-            39'h1000 : ALUoutput <= (rs1 | imm);                                  //ori
-            39'h2000 : ALUoutput <= (rs1 & imm);                                  //andi
-            39'h4000 : ALUoutput <= (rs1 << imm[4:0]);                            //slli
-            39'h8000 : ALUoutput <= (rs1 >> imm[4:0]);                            //srli
-            39'h10000 : ALUoutput <= (rs1 > imm[4:0]);                            //srai
-            39'h20000 : ALUoutput <= (rs1 < imm)?1:0;                             //slti
-            39'h40000 : ALUoutput <= (rs1 < imm)?1:0;                             //sltiu
+            46'h1 : ALUoutput = rs1 + rs2;                                       //add
+            46'h2 : ALUoutput <= rs1 - rs2;                                       //sub
+            46'h4 : ALUoutput <= rs1 ^ rs2;                                       //xor
+            46'h8 : ALUoutput <= rs1 | rs2;                                       //or
+            46'h10 : ALUoutput <= rs1 & rs2;                                      //and
+            46'h20 : ALUoutput <= rs1 << rs2;                                     //sll
+            46'h40 : ALUoutput <= rs1 >> rs2;                                     //srl
+            46'h80 : ALUoutput <= rs1 > rs2;                                      //sra 
+            46'h100 : ALUoutput <= (rs1 > rs2)?1:0;                               //slt
+            46'h200 : ALUoutput <= (rs1 > rs2)?1:0;                               //sltu
+            46'h400 : ALUoutput <= (rs1 + imm);                                   //addi
+            46'h800 : ALUoutput <= (rs1 ^ imm);                                   //xori
+            46'h1000 : ALUoutput <= (rs1 | imm);                                  //ori
+            46'h2000 : ALUoutput <= (rs1 & imm);                                  //andi
+            46'h4000 : ALUoutput <= (rs1 << imm[4:0]);                            //slli
+            46'h8000 : ALUoutput <= (rs1 >> imm[4:0]);                            //srli
+            46'h10000 : ALUoutput <= (rs1 > imm[4:0]);                            //srai
+            46'h20000 : ALUoutput <= (rs1 < imm)?1:0;                             //slti
+            46'h40000 : ALUoutput <= (rs1 < imm)?1:0;                             //sltiu
+            46'h10000000000 : ALUoutput <= rs1 * rs2;                              //mul
+            46'h20000000000 : ALUoutput <= {rs1 * rs2} >> 32;                     //mulh
+            46'h40000000000 :ALUoutput <= {{32'd0, rs1} * rs2} >> 32;             //mulhu
+            46'h80000000000 : ALUoutput <= {rs1 * rs2} >> 32;                     //mulhsu
+            46'h100000000000 : ALUoutput <= rs1 / rs2;                             //div
+            46'h200000000000 : ALUoutput <= rs1 / rs2;                            //divu
+            46'h400000000000 : ALUoutput <= rs1 % rs2;                            //rem
+            46'h800000000000 : ALUoutput <= rs1 % rs2;                            //remu
+               
         endcase
 end       
 endmodule
